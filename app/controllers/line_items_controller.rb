c4968df7a73c6c909a4fe9264749eb97a5d89e0e
Ray Mulligan <rmullig2@optonline.net>
2017-02-07 12:21:24 -0500

    Done.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,10 +1,14 @@
 class LineItemsController < ApplicationController
   
   def create
+    binding.pry
     if current_user.current_cart.nil?
       current_user.current_cart = Cart.create(user_id: current_user.id)
+      current_user.save
     end
+    #binding.pry
     current_user.current_cart.line_items << LineItem.create(item_id: params[:item_id], cart_id: current_user.current_cart.id)
+    redirect_to cart_path(current_user.current_cart.id)
     #binding.pry
   end
 
