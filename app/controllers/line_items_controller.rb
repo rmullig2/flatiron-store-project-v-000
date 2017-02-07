55ddeb2214ef7e78388162f620a2be0d26c92cf0
Ray Mulligan <rmullig2@optonline.net>
2017-02-07 16:00:49 -0500

    merge

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -5,7 +5,7 @@ class LineItemsController < ApplicationController
       current_user.current_cart = Cart.create(user_id: current_user.id)
     end
     current_user.current_cart.line_items << LineItem.create(item_id: params[:item_id], cart_id: current_user.current_cart.id)
-    binding.pry
+    #binding.pry
   end
 
 end
